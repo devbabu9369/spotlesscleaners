cd92b4603ee69bd9445a6591cc738012e7455537
devendra.pratap <devendra.pratap@thinkexam.com>
2024-11-20 15:34:15 +0530

    update wordpress

--- resources/views/admin/dashboard/common/footer.blade.php
+++ resources/views/admin/dashboard/common/footer.blade.php
@@ -3,4 +3,7 @@
     <p>© <?php echo date('Y'); ?> Devendra Spotless Cleaners, All rights reserved | Powered by spotless Cleaners.com by <a href="https://spotlesscleaners.in"> Spotless Cleaners</a></p> 
     
   	</footer>
+    <div>
+      <h1>footer</h1>
+    </div>
   </div>
